bef2020b2de1dba525e405b1e75f36a28245d892
Reza <radhitya@uwaterloo.ca>
2018-10-11 10:26:17 -0400

    hgddhassad

---
+++ python_scripts/histogram_sdf.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pylab as plt
+#import scipy.stats as stats
+import pandas as pd
+
+import useful_things as ut
+
+r_vals = np.arange(0.2, 21, 0.2);
+
+# cuz area fraction
+r_vals = np.insert(r_vals, 0, 0)
+#r_vals.insert(0, 0);
+
+
+"""
+fig1 = plt.figure(1);
+fig1.patch.set_facecolor('white')
+plt.clf();
+ax = plt.gca();
+
+plt.plot(r_vals, np.asarray(c_vals_cf_bad), 'r');
+#plt.plot(r_vals, np.asarray(c_vals_1), 'b');
+
+plt.title(r"Cov Bad");
+plt.show();
+
+fig2 = plt.figure(2);
+fig2.patch.set_facecolor('white')
+plt.clf();
+ax = plt.gca();
+
+plt.plot(r_vals, np.asarray(c_vals_cf_good), 'b');
+#plt.plot(r_vals, np.asarray(c_vals_1), 'b');
+
+plt.title(r"Cov Good");
+plt.show();
+"""
+
+fig3 = plt.figure(3);
+fig3.patch.set_facecolor('white')
+plt.clf();
+ax = plt.gca();
+
+# logarithmic y-axis
+#ax.set_yscale('log')
+
+#plt.plot(r_vals, np.asarray(scf_vals_balabolka_01), '#2FADF4', linewidth=1);
+#plt.plot(r_vals, np.asarray(scf_vals_balabolka_man2x), '#ED2B2B', linewidth=1);
+#plt.plot(np.asarray(scf_vals_balabolka_01), '#2FADF4', linewidth=1);
+#plt.plot(np.asarray(scf_vals_balabolka_man2x),  '#ED2B2B', linewidth=1);
+
+plt.hist(np.asarray(scf_vals_balabolka_01), 10, normed=True);
+plt.hist(np.asarray(scf_vals_balabolka_man2x), 10, normed=True);
+
+plt.title(r"Compare");
+plt.show();

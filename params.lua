c08fa853bf06ded3767b4242552a5e332451c5ef
Reza <radhitya@uwaterloo.ca>
2018-12-18 17:02:39 -0500

    ggg

--- params.lua
+++ params.lua
@@ -103,8 +103,8 @@ _dt = 0.05;    --- do not set this higher than 0.1
 --- _noise_map_update_time = 10000000; --- in ms (disabled)
 
 --- Force parameters
-_k_neg_space_edge      = 60;	--- edge force for springs
 _k_edge                = 10;	--- edge force for filling elements
+_k_neg_space_edge      = 60;	--- edge force for springs
 _k_edge_small_factor   = 24;
 _k_repulsion           = 40;	--- repulsion force
 _repulsion_soft_factor = 1.0;	--- soft factor for repulsion force

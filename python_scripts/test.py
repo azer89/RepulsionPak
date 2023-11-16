8e4ccc265d2cd470a731e755c29c98cd61d4d9a9
Reza Saputra <radhitya@uwaterloo.ca>
2023-11-15 19:46:58 -0500

    removed unused python files

--- python_scripts/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pylab as plt
-#import scipy.stats as stats
-import pandas as pd
-
-import useful_things as ut
-
-r_vals = np.arange(0, 1, 0.1);
-r_vals = r_vals[:5]
